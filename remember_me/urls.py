6b2859a989faf42417d459b2ce4701cb053488a8
Justyna Thompson <justynabp@gmail.com>
2024-11-22 11:15:39 +0000

    Download summernote

--- remember_me/urls.py
+++ remember_me/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("memories.urls"), name="memories-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("memories.urls"), name="memories-urls"),
 ]
