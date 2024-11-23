5b022cc5cf6f56bb110e28e631549bb272860400
Justyna Thompson <justynabp@gmail.com>
2024-11-23 21:52:52 +0000

    Update setting after redownloading packages

--- remember_me/settings.py
+++ remember_me/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-justynath-rememberme-nb2xqdamwen.ws.codeinstitute-ide.net", '.herokuapp.com']
+ALLOWED_HOSTS = ["8000-justynath-rememberme-6k2r27hnhgd.ws.codeinstitute-ide.net", '.herokuapp.com']
 
 
 # Application definition
