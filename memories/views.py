d36fd2bdbd94d9fb5cb18d38a9225fb2def0a280
Justyna Thompson <justynabp@gmail.com>
2024-12-18 13:37:56 +0000

    Style and test 404 and 500 pages

--- memories/views.py
+++ memories/views.py
@@ -178,8 +178,8 @@ class RemoveFromFavouritesView(LoginRequiredMixin, generic.View):
       
 def custom_404(request, exception):
     """ Custom 404 Error Page """
-    return render(request, '404.html', status=404)
+    return render(request, 'memories/404.html', status=404)
 
 def custom_500(request):
     """ Custom 500 Error Page """
-    return render(request, '500.html', status=500)
+    return render(request, 'memories/500.html', status=500)
