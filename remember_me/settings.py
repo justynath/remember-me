62a79a12a4771bd51c665b424f0eaaeaaafd03ae
Justyna Thompson <justynathompson@Justynas-Laptop.local>
2025-05-28 17:25:15 +0100

    Fix database issues. Upload new fixtures

--- remember_me/settings.py
+++ remember_me/settings.py
@@ -114,8 +114,15 @@ WSGI_APPLICATION = 'remember_me.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 if 'test' in sys.argv:
